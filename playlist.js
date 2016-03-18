cf09539d90c78bfcae046e4f136f0d0ea088e25e
Brett <brettrheenan@gmail.com>
2016-03-18 15:57:27 +0100

    Done.

--- playlist.js
+++ playlist.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function addToPlaylist(object, song, artist) {
+	object[artist] = song;
+	return object;
+}
+
+function removeFromPlaylist(object, artist) {
+	delete object[artist];
+	return object;
+}
+
+function changePlaylistSong(object, song, artist) {
+	object[artist] = song;
+	return object;
+}
+
+function readPlaylist(object) {
+	if (Object.keys(object).length === 0) {
+		console.log("your playlist is empty"); 
+	} else {
+		for ( var key in object) {
+			console.log("The song " + object[key] + " by " + key + " is in your playlist");
+		}
+	}
+}
