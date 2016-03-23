a53e5b3fabd75cf9cd4ad4ba2b118e928dded4e8
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-23 10:15:02 -0600

    Done.

--- playlist.js
+++ playlist.js
@@ -1,1 +1,28 @@
 'use strict';
+
+var myPlaylist = {};
+
+function addToPlaylist(myPlaylist, song, artist) {
+  myPlaylist[artist] = song;
+  return myPlaylist;
+}
+
+function removeFromPlaylist(myPlaylist, artist) {
+  delete myPlaylist[artist];
+  return myPlaylist;
+}
+
+function changePlaylistSong(myPlaylist, artist, song) {
+  myPlaylist[song] = artist;
+  return myPlaylist;
+}
+
+function readPlaylist(myPlaylist) {
+  if (Object.keys(myPlaylist).length == 0){
+    console.log("your playlist is empty");
+  } else {
+    for (var key in myPlaylist) {
+      console.log("The song " + myPlaylist[key] + " by " + key + " is in your playlist");
+    }
+  }
+}
