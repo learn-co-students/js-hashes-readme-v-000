0f5b4936c2c44866efc60236a586ce6686dcedd6
rocketNeck <homestarflyer@gmail.com>
2016-05-02 19:34:05 -0400

    Done.

--- playlist.js
+++ playlist.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function addToPlaylist(playlist, song, artist){
+  playlist[artist] = song;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
+
+function changePlaylistSong(playlist, newSong, artistName) {
+  playlist[artistName] = newSong;
+  return playlist;
+}
+
+function readPlaylist(playlist){
+  if (Object.keys(playlist).length === 0){
+    console.log('your playlist is empty');
+  }else {
+    for (var key in playlist){
+      console.log("The song " + playlist[key] + " by " + key + " is in your playlist");
+    }
+  }
+}
