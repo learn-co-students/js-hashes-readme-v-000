178454c38d398f411d3830885d0f4ee12995517c
cernanb <cernanb@gmail.com>
2016-05-02 17:03:39 -0700

    created and manipulated hashes in javascript

--- playlist.js
+++ playlist.js
@@ -1,1 +1,29 @@
 'use strict';
+
+var myPlayList = {};
+
+function addToPlaylist(playList, song, artist){
+  playList[artist] = song;
+  return playList;
+}
+
+function removeFromPlaylist(playList, artist){
+  delete playList[artist];
+  return playList;
+}
+
+function changePlaylistSong(playList, newSong, artist){
+  playList[artist] = newSong;
+  return playList;
+}
+
+function readPlaylist(playList){
+  if (Object.keys(playList).length == 0){
+    console.log("your playlist is empty")
+  }
+  else {
+    for(var key in playList){
+      console.log("The song " + playList[key] + ' by ' + key + ' is in your playlist');
+    }
+  }
+}
