a17fb78e14a81ffeca1bf6d78e26502a5eb6c016
genericlady <yiannakokalas@gmail.com>
2016-05-13 10:04:53 -0400

    Done.

--- playlist.js
+++ playlist.js
@@ -1,1 +1,32 @@
 'use strict';
+
+var playlist = {};
+
+function addToPlaylist(playlist, song, artist) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
+
+function changePlaylistSong(playlist, song, artist ) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function readPlaylist(playlist) {
+  if (playlistLength(playlist) == 0) {
+    console.log('your playlist is empty');
+  } else {
+    Object.keys(playlist).forEach(function (artist) {
+      console.log('The song ' + playlist[artist] + ' by ' + artist + ' is in your playlist');
+    });
+  }
+}
+
+function playlistLength(playlist) {
+  return Object.keys(playlist).length;
+}
