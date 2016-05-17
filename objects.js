ee224429974543daf58182e46d630355a746eb89
gormanjp <jpgorman8@gmail.com>
2016-05-17 00:12:21 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: 'song'};
+
+function updatePlaylist(playlist, name, title){
+	playlist[name] = title;
+	return playlist;
+}
+
+function removeFromPlaylist(playlist, name){
+	delete playlist[name];
+	return playlist;
+}
