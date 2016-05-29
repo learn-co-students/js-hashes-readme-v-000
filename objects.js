100d7dba7070b3b48a1910dd4fddd6f0a5f13f83
davdkm <davd.km@gmail.com>
2016-05-29 12:34:51 -0700

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var playlist = {andrewYork: 'Sunburst'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
