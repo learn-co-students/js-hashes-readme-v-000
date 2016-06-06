e28c5a16cbdd9d8a18e5d4f78a138acbf4c48e5b
James Novak <jamesnovak90@gmail.com>
2016-06-06 14:49:33 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: "artistname", song: "songname"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
