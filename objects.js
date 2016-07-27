3ba2fbe0220a6528fdd66db685ba21aa80e536f8
Jason Casares <jasonc@csdrafting.com>
2016-07-27 06:03:23 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,14 @@
-var playlist = []
+var playlist = {someone: 'something'}
 
 function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
 
 }
