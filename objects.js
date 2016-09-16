8e7cd0ec81302ab65849d610667f13f04ee28d17
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-16 10:30:10 -0400

    donzo

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {rumpsringa: "shake em loose tonight"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
