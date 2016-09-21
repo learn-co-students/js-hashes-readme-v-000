6019f9d7fd5fcbf3c5d57b2d30488d12855ec6d2
kentleong <kentleong@github.com>
2016-09-21 01:01:27 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { someone: 'something' }
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
