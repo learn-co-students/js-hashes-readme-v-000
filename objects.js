d66478a4f378df64975b3b79c67d73179e784950
tpstar <hanlee12@gmail.com>
2016-10-07 20:51:07 -0500

    done

--- objects.js
+++ objects.js
@@ -1,4 +1,11 @@
+var playlist = {kanye: "Stronger", girls_generation: "Happy"}
+
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
 }
