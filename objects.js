44541149b7102466aa9c543929d9d7344b58a924
Christian Whitesides <whitesidescr@gmail.com>
2016-10-12 15:28:55 -0600

    Add spacing to updatePlaylist method

--- objects.js
+++ objects.js
@@ -2,10 +2,12 @@ var playlist = { mj: "Billy Jean" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
+  
   return playlist;
 }
