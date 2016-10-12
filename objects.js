7ca348cad93ee87d76b1d22215131cf83b4cd2fd
Christian Whitesides <whitesidescr@gmail.com>
2016-10-12 15:27:45 -0600

    Refactor updatePlaylist method

--- objects.js
+++ objects.js
@@ -1,16 +1,8 @@
-var playlist = {mj: "Billy Jean"};
+var playlist = { mj: "Billy Jean" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  if (Object.keys(playlist).length === 0) {
-    var currentPlaylist = playlist;
-    currentPlaylist[artistName] = songTitle;
-    return currentPlaylist;
-  } else {
-    for (var key in playlist) {
-      key === artistName ? playlist[key] = songTitle : playlist[artistName] = songTitle;
-    }
-    return playlist;
-  }
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
