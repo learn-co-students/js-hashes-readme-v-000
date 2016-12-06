96cdc101305ebf6930eb47a5e6ceccc9fbe9779d
ldgarber <leah.garber88@gmail.com>
2016-12-06 16:09:47 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: "song name"}; 
+
+var updatePlaylist = function(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle; 
+  return playlist; 
+}
+
+var removeFromPlaylist = function(playlist, artistName) {
+  delete playlist[artistName]; 
+  return playlist; 
+}
