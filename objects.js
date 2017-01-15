0f8fd584481b07054b1c98c67b93fcdbe9abc7b5
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-15 15:56:22 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+// var playlist = newObject();
+
+function updatePlaylist(playlist, artist, title){
+
+  playlist[artist] = title
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
