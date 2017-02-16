a021d1c8bccbf63f981548019bab61e8072154e6
smithWEBtek <brad@smithwebtek.com>
2017-02-16 12:51:55 -0500

    Done.

---
+++ objects_lab.js
@@ -0,0 +1,52 @@
+// objects_lab.js
+// var foods = ["bread", "nuts", "peas", "greens", "french fries", "nachos", "okra"] 
+
+// var meals = {
+//   breakfast: "oatmeal",
+//   lunch: "bean soup",
+//   dinner: "chef salad"
+// }; 
+
+// function addFoods(arr) {
+// for (var i = 0, len = arr.length; i < len; i++) {
+//   meals["snack" + (i+1)] = arr[i];
+// }
+// return meals;
+// } 
+
+// function showKeyValues(list) {
+//   var keyz = Object.keys(list);
+//   for (var i = keyz.length - 1; i >= 0; i--) {   
+//     console.log("For "+ keyz[i] + ", " + "I had " + list[keyz[i]] + ".");
+//    }
+// }
+
+// function showKeyValues2(list) {
+//   for (var key in list)
+// console.log("for " + key + " I ate " + meals[key]);
+// }
+
+// var playlist = {};
+// var playlist1 = new Object();
+// var playlist2 = {};
+// var params1 = ["playlist1", "Neo", "Bite My Love Pump"]
+// var params2 = ["playlist2", "Shakira", "Spit it out"]
+
+// function updatePlaylist(playlist, artist_name, song_title) {
+//     var list = playlist;
+//     list[artist_name] = song_title; 
+//     return list;
+// }
+
+
+// function removeFromPlaylist(playlist, artist_name) {
+//     playlist[artist_name].delete; 
+//     return playlist1;
+// }
+
+// function updatePlaylist1(artist_name, song_title) {
+//     playlist1[artist_name] = song_title; 
+//     return playlist1;
+// }
+
+
