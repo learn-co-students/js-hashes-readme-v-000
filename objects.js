5b7a76964301fd6b66b007d51754ca8dbd5060f8
github username <github email address>
2019-01-26 06:48:57 -0500

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  for (var artist in playlist) {
+    playlist[artist]:song
+  }
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+delete playlist[artist]
+  return playlist
+}
